2a317fa40e6c3cb0704216b91b81b5a8331c31e1
Magesh Kuppan <tkmagesh77@gmail.com>
2022-02-01 14:05:27 +0530

    assignment-4 problem statement

--- 04-print-file/print-file-stream.js
+++ 04-print-file/print-file-stream.js
@@ -1,12 +1,22 @@
 var fs = require('fs')
 
 var stream = fs.createReadStream('./sample.txt', { encoding : 'utf8', highWaterMark : 32 * 1024 })
+var readCount = 0
 
-/* 
 stream.on('data', function(chunk){
+    readCount++
     console.log(chunk)
 })
 
+stream.on('end', function(){
+    console.log('Job Done! read counts = ', readCount);
+});
+stream.on('end', function(){
+    console.log('Job Done! read counts = ', readCount);
+});
+stream.on('end', function(){
+    console.log('Job Done! read counts = ', readCount);
+});
 stream.on('end', function(){
     console.log('Job Done! read counts = ', readCount);
 });
@@ -14,7 +24,7 @@ stream.on('end', function(){
 stream.on('error', function(err){
     console.log("Error : ", err.message)
 }) 
-*/
+
 
 //using the 'pipe' method
 /* 
@@ -31,5 +41,5 @@ stream.pipe(process.stdout)
 */
 
 //creating a copy
-copyStream = fs.createWriteStream("sample-copy.txt")
-stream.pipe(copyStream)
+/* copyStream = fs.createWriteStream("sample-copy.txt")
+stream.pipe(copyStream) */
