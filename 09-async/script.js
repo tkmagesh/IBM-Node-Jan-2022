d9085e119f0435cc628bfdf478650464ec53ef7f
Magesh Kuppan <tkmagesh77@gmail.com>
2022-02-02 16:09:19 +0530

    async sequencing - assignment

---
+++ 09-async/script.js
@@ -0,0 +1,67 @@
+(function(){
+    function f1Sync(){
+        console.log('f1Sync started');
+        console.log('f1Sync completed');
+    }
+
+    function f2Sync(){
+        console.log('f2Sync started');
+        console.log('f2Sync completed');
+    }
+
+    function f3Sync(){
+        console.log('f3Sync started');
+        console.log('f3Sync completed');
+    }
+
+    function f4Sync(){
+        console.log('f4Sync started');
+        console.log('f4Sync completed');
+    }
+
+    function runSync(){
+        f1Sync()
+        f2Sync()
+        f3Sync()
+        f4Sync()
+    }
+
+    window['runSync'] = runSync
+
+    function f1Async(){
+        console.log('f1Async started');
+        setTimeout(function(){
+            console.log('f1Async completed');
+        },700)
+    }
+
+    function f2Async(){
+        console.log('f2Async started');
+        setTimeout(function(){
+            console.log('f2Async completed');
+        },300)
+    }
+
+    function f3Async(){
+        console.log('f3Async started');
+        setTimeout(function(){
+            console.log('f3Async completed');
+        },400)
+    }
+
+    function f4Async(){
+        console.log('f4Async started');
+        setTimeout(function(){
+            console.log('f4Async completed');
+        },500)
+    }
+
+    function runAsync(){
+        f1Async()
+        f2Async()
+        f3Async()
+        f4Async()
+    }
+
+    window['runAsync'] = runAsync
+})()
