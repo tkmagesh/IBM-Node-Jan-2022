17539fe1fc4e4f88ab5f0d629e6a9652a29ca1c5
Magesh Kuppan <tkmagesh77@gmail.com>
2022-02-02 10:58:12 +0530

    assignment-6 solution

--- 05-web-server/server.js
+++ 05-web-server/server.js
@@ -1,13 +1,34 @@
-var http = require('http')
+
+var http = require('http'),
+    fs = require('fs'),
+    path = require('path');
 
 var server = http.createServer(function(req, res){
-    console.log(req.url)
-    res.write("Welcome to node.js")
-    res.end()
+   console.log(req.method, req.url)
+   var resourceName = req.url === '/' ? '/index.html' : req.url;
+   var resourceRequested = path.join(__dirname, resourceName);
+   if (!fs.existsSync(resourceRequested)){
+       res.statusCode = 404;
+       res.end()
+       return
+   }
+   /* 
+   fs.readFile(resourceRequested, function(err, fileContents){
+       if (err){
+           console.log(err)
+           res.statusCode = 500;
+           res.end()
+           return
+       }
+       res.write(fileContents)
+       res.end()
+   })*/
+   fs.createReadStream(resourceRequested).pipe(res)
 })
 
 server.listen(8080)
 server.on('listening', function(){
     console.log('server listening on port - ', 8080)
+    console.log('ready to serve resources from : ', __dirname)
 })
 
