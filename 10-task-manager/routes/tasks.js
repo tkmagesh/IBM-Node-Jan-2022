d9a56a4e869dc946d13523169dd0da92ba151fba
Magesh Kuppan <tkmagesh77@gmail.com>
2022-02-03 15:04:24 +0530

    assignment-12 problem statement

--- 10-task-manager/routes/tasks.js
+++ 10-task-manager/routes/tasks.js
@@ -11,13 +11,25 @@ router.get('/', function(req, res, next){
 });
 
 router.post('/', function(req, res, next){
-    console.log(req.body);
-    res.send('Task added')
+    console.log(req.body); //access the req body deserialized from json string
+    //fill in the blanks
+    //res.send('Task added')
+    //id = max id + 1
+    //send the newly created task with status code 201
 });
 
 router.get('/:id', function(req, res, next){
-    console.log('Serving task with id ', req.params.id)
+    console.log('Serving task with id ', req.params.id) //access the route parameter
+    console.log(req.query)
     res.json(taskList);
 });
+
+router.put('/:id', function (req, res, next){
+
+});
+
+router.delete('/:id', function(req, res, next){
+    
+})
 
 module.exports = router;
