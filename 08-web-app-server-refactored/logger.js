f325a5369b498883e5c0d602afd13f122feff3b6
Magesh Kuppan <tkmagesh77@gmail.com>
2022-02-03 12:33:44 +0530

    using nodemon

--- 08-web-app-server-refactored/logger.js
+++ 08-web-app-server-refactored/logger.js
@@ -5,7 +5,7 @@ module.exports = function(req, res, next){
     res.on('finish', function(){
         var end = new Date();
         var elapsed = end - start;
-        console.log(chalk.blue(req.method) + "\t" + chalk.red(req.url) + "\t" + chalk.yellow(res.statusCode) + "\t" + elapsed)
+        console.log(chalk.green(req.method) + "\t" + chalk.red(req.url) + "\t" + chalk.yellow(res.statusCode) + "\t" + elapsed)
     })
     
     return next()
