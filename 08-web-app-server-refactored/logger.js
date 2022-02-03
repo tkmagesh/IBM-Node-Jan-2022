eac09b7ede6ab966e904fda7c3100d097d349a5d
Magesh Kuppan <tkmagesh77@gmail.com>
2022-02-03 12:21:59 +0530

    using npm

--- 08-web-app-server-refactored/logger.js
+++ 08-web-app-server-refactored/logger.js
@@ -1,9 +1,11 @@
+var chalk = require('chalk')
+
 module.exports = function(req, res, next){
     var start = new Date();
     res.on('finish', function(){
         var end = new Date();
         var elapsed = end - start;
-        console.log(req.method + "\t" + req.url + "\t" + res.statusCode + "\t" + elapsed)
+        console.log(chalk.blue(req.method) + "\t" + chalk.red(req.url) + "\t" + chalk.yellow(res.statusCode) + "\t" + elapsed)
     })
     
     return next()
