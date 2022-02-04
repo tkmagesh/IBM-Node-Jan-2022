08dbc59577a2349e888565a42f326b5599b3debf
Magesh Kuppan <tkmagesh77@gmail.com>
2022-02-04 16:57:48 +0530

    graphql server demo

--- 14-Socket-IO/index.js
+++ 14-Socket-IO/index.js
@@ -8,8 +8,8 @@ app.get('/', function(req, res){
 });
 
 io.on('connection', function(socket){
-  socket.on('chat message', function(msg){
-    io.emit('chat message', msg);
+  socket.on('clientMessage', function(msg){
+    io.emit('serverMessage', msg);
   });
 });
 
